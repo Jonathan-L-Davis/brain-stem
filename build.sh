d2233ad0e13304a6752d0c3a343fc22d009d59c7
Jon Davis <jldavisjr@yahoo.com>
2024-11-11 10:55:56 -0500

    Won't build now. Build script searches for C++17. Need to move back from C++20, since Zaratan's g++ version doesn't support C++20 proper yet.

--- build.sh
+++ build.sh
@@ -2,6 +2,32 @@
 
 Source_Files=( "main.cpp" )
 Object_Files=()
+Compiler=""
+
+if [[ ! -z $(command -v "clang++") ]]; then
+    if [[ -z $(clang++ --std=c++terriblearg -c src/main.cpp 2>&1 | grep c++17 ) ]] ; then # this check is compiler dependent. Clang emits correct/supported standards when given a bad one.
+        echo "Installed Clang version does not support c++17"
+    else
+        Compiler="clang++"
+    fi
+else
+    echo "clang++ not found. Looking for g++."
+fi
+
+if [[ -z "$Compiler" && ! -z $(command -v "g++") ]] ; then
+    if [[ -z $(g++ -v --help 2> /dev/null | sed -n '/^ *-std=\([^<][^ ]\+\).*/ {s//\1/p}' | grep c++17) ]] ; then # THX https://stackoverflow.com/users/616460/jason-c with https://stackoverflow.com/questions/34836775/compiler-standards-support-c11-c14-c17
+        echo "Installed g++ version does not support c++17"
+    else
+        Compiler="g++"
+    fi
+elif [[ -z "$Compiler" ]] ; then
+    echo "g++ not found. Giving up."
+fi
+
+if [[ -z $Compiler ]] ; then
+    echo "Could not find suitable compiler"
+    exit
+fi
 
 mkdir -p obj
 for file in "${Source_Files[@]}" ; do
@@ -18,11 +44,11 @@ for file in "${Source_Files[@]}" ; do
     #echo "$file ---> $object_file"
     Object_Files+=( "${object_file}" )
     mkdir -p "obj/$path" # otherwise clang++/g++ complain about non-existing directory
-    clang++ "$file" -c -o "${object_file}" -std=c++20
+    $Compiler "$file" -c -o "${object_file}" -std=c++17
 done
 
 for object in "${Object_Files[@]}" ; do
     file_glob+=" $object"
 done
 
-clang++ $file_glob -o "brain-stem"
+$Compiler $file_glob -o "brain-stem"
